d2e5b2b20eaedf4359ffa648a5b2eb980f174ef5
luingalad <ertugoguz@gmail.com>
2022-10-16 03:32:20 +0300

    setup.sh fix

--- setup.sh
+++ setup.sh
@@ -2,5 +2,9 @@
 
 sudo apt-get update
 sudo apt-get install -y pip
+sudo apt-get install -y wget
 
 pip3 install -r requirements.txt
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
